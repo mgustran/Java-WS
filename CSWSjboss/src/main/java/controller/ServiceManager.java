ad0797bc4e7a2caec484566d3b0cc8e62b4f68b3
Miquel Gustran <mgustran@hiberus.com>
2016-05-20 14:00:15 +0200

    añadido mantenimiento CS web service

--- CSWSjboss/src/main/java/controller/ServiceManager.java
+++ CSWSjboss/src/main/java/controller/ServiceManager.java
@@ -1,24 +1,32 @@
 package controller;
+/*
+import model.SpeedUnit;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- * Created by mgustran on 20/05/2016.
- */
-public class ServiceManager {
-    public void connectServive() throws MalformedURLException {
-        URL wsdlDocumentLocation = new URL("http://www.webservicex.net/ConvertSpeed.asmx?WSDL");
-        String namespaceURI = "http://www.webserviceX.NET/";
-        String servicePart = "ConvertSpeeds";
-        String portName = "ConvertSpeedsSoap";
-        QName serviceQN = new QName(namespaceURI, servicePart);
-        QName portQN = new QName(namespaceURI, portName);
 
-        // Instancia del servicio WEB
-        Service service = Service.create(wsdlDocumentLocation, serviceQN);
-        ConvertSpeedsSoap convertSpeedsSoap = service.getPort(portQN, ConvertSpeedsSoap.class);
-    }
+public class ServiceManager {
+
+        public void service() throws MalformedURLException {
+
+                URL wsdlDocumentLocation = new URL("http://www.webservicex.net/ConvertSpeed.asmx?WSDL");
+                String namespaceURI = "http://www.webserviceX.NET/";
+                String servicePart = "ConvertSpeeds";
+                String portName = "ConvertSpeedsSoap";
+                QName serviceQN = new QName(namespaceURI, servicePart);
+                QName portQN = new QName(namespaceURI, portName);
+
+                // Instancia del servicio WEB
+                Service service = Service.create(wsdlDocumentLocation, serviceQN);
+                ConvertSpeedsSoap convertSpeedsSoap = service.getPort(portQN, ConvertSpeedsSoap.class);
+
+                Double torna = convertSpeedsSoap.convertSpeed(12.12, SpeedUnit.KNOTS, SpeedUnit.KILOMETERS_PERHOUR);
+                Double expected = 22.444;  // Result: 22.44624 */
+
+
+
+}
 }
--- CSWSjboss/src/main/java/controller/mAAIN.java
+++ CSWSjboss/src/main/java/controller/mAAIN.java
@@ -1,5 +1,9 @@
 package controller;
 
+/**
+ * Created by mgustran on 20/05/2016.
+ */
+
 import model.SpeedUnit;
 
 import javax.xml.namespace.QName;
@@ -8,36 +12,32 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
- * Created by mgustran on 20/05/2016.
+ * Created by mgustran on 13/05/2016.
  */
-public class Controller {
-    private Double inputSpeed;
-    private Double outputSpeed;
-    private SpeedUnit fromUn;
-    private SpeedUnit toUn;
 
+public class mAAIN {
 
+    public static void main(String[] args) {
 
-
-    public Double convert() throws MalformedURLException {
-        URL wsdlDocumentLocation = new URL("http://www.webservicex.net/ConvertSpeed.asmx?WSDL");
+        URL wsdlDocumentLocation = null;
+        try {
+            wsdlDocumentLocation = new URL("http://www.webservicex.net/ConvertSpeed.asmx?WSDL");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         String namespaceURI = "http://www.webserviceX.NET/";
         String servicePart = "ConvertSpeeds";
         String portName = "ConvertSpeedsSoap";
         QName serviceQN = new QName(namespaceURI, servicePart);
         QName portQN = new QName(namespaceURI, portName);
 
-        // Instancia del servicio WEB
         Service service = Service.create(wsdlDocumentLocation, serviceQN);
         ConvertSpeedsSoap convertSpeedsSoap = service.getPort(portQN, ConvertSpeedsSoap.class);
 
-        Double outputSpeed = convertSpeedsSoap.convertSpeed(inputSpeed, fromUn, toUn);
-        Double expected = 22.444;  // Result: 22.44624
+        Double torna = convertSpeedsSoap.convertSpeed(12.12, SpeedUnit.KNOTS, SpeedUnit.KILOMETERS_PERHOUR);
 
-        return outputSpeed;
+        System.out.println(torna);
 
 
     }
-
-
 }
