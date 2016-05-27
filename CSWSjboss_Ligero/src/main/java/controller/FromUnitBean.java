aa3b01002826b3100b2347444c4fdddc61185871
Miquel Gustran <mgustran@hiberus.com>
2016-05-27 13:26:51 +0200

    creada copia ligera proyecto CSWSjboss

---
+++ CSWSjboss_Ligero/src/main/java/controller/FromUnitBean.java
@@ -0,0 +1,32 @@
+package controller;
+
+import model.SpeedUnit;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.SelectItem;
+import java.io.Serializable;
+
+/**
+ * Created by mgustran on 24/05/2016.
+ */
+@ManagedBean(name="FromUnitBean")
+@SessionScoped
+public class FromUnitBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private SpeedUnit fromUnitBean ;
+
+    public SpeedUnit getFromUnit() {
+        return fromUnitBean;
+    }
+
+    public void setFromUnit(SpeedUnit fromUnitBean) {
+        this.fromUnitBean = fromUnitBean;
+    }
+
+
+
+
+}
